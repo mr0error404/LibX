68739ae859a68e1c23c4aa0434bb175e9374182f
Abdullah Yasein <101466323+mr0error404@users.noreply.github.com>
2024-06-02 11:31:51 +0300

    addClassesInModel

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace XLIB.Models
 {
 	public class ApplicationDbContext : DbContext
 	{
-		public ApplicationDbContext()
-		{
-		}
-	}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookState> BookStates { get; set; }
+        public DbSet<Laibrarian> Laibrarians { get; set; }
+        public DbSet<MainCategory> MainCategories { get; set; }
+        public DbSet<Manager> Manager { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
 }
 
---
+++ Models/SubCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace XLIB.Models
+{
+    public class SubCategory
+    {
+        // Properties
+        [Key]
+        public int SubcategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string SubcategoryName { get; set; }
+
+        // Foreign Key
+        [ForeignKey("MainCategory")]
+        public int MainCategoryId { get; set; }
+        public MainCategory MainCategory { get; set; }
+    }
+}
+
